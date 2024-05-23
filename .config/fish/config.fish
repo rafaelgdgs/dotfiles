da216a88d793ff5e150a9baaa8b55eec2b1eef04
rafaelgdgs <rafagsique@hotmail.com>
2024-05-23 04:21:14 -0300

    feat(fish): added icons to ls command

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,8 +3,9 @@ if status is-interactive
 end
 
 set -gx EDITOR nvim
-set -gx __fish_ls_command eza
+set -gx __fish_ls_command eza --icons
 #eval "$(fzf --fish)"
 set -gx RANGER_LOAD_DEFAULT_RC FALSE
+set -gx SSH_AUTH_SOCK $XDG_RUNTIME_DIR/gcr/ssh
 
 starship init fish | source
